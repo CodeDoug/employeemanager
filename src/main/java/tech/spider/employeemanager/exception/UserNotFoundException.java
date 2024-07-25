842659dac55c36c2087129f0c6b61df4d942b99e
Douglas Sarmento <devdoug.it@gmail.com>
2024-07-25 14:28:30 -0300

    Add files via upload

---
+++ src/main/java/tech/spider/employeemanager/exception/UserNotFoundException.java
@@ -0,0 +1,7 @@
+package tech.spider.employeemanager.exception;
+
+public class UserNotFoundException extends RuntimeException {
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+}
